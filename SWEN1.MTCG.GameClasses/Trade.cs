582535f6ebc9a012ac096f04ccdbac3fd40f45e4
Demyst1fy <demyst1fy@outlook.com>
2021-10-27 17:11:42 +0200

    Initial commit

---
+++ SWEN1.MTCG.GameClasses/Trade.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SWEN1.MTCG.ClassLibrary
+{
+    public class Trade
+    {
+        public string Username { get; }
+        public string Id { get; }
+        public string CardToTrade { get; }
+        public string Type { get; }
+        public double MinimumDamage { get; }
+
+        public Trade(string username, string id, string cardToTrade, string type, double minimumDamage)
+        {
+            Username = username;
+            Id = id;
+            CardToTrade = cardToTrade;
+            Type = type;
+            MinimumDamage = minimumDamage;
+        }
+    }
+}
